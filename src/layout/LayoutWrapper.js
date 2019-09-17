39e67c2c8dbbb30870da79d52a8772616611a438
fmaguinaa <fmaguinaa@gmail.com>
2019-09-17 10:12:44 -0500

    btn-up

--- src/layout/LayoutWrapper.js
+++ src/layout/LayoutWrapper.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { Layout } from 'antd'
 import Footer from '../common/footer/Footer'
 import Header from '../common/header/Header'
-
+import BtnUp from '../common/btnUp/BtnUp'
 // import {Link, withRouter} from 'react-router-dom'
 
 // import * as paths from '../routes/paths'
@@ -18,6 +18,7 @@ export default class LayoutBase extends Component {
                     {this.props.children}
                 </Content>
                 <Footer />
+                <BtnUp />
             </Layout>
         )
     }
